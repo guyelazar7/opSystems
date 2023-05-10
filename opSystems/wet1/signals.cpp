c21771856901466f17b0b79e9f7f8ce8d0188ab2
guyelazar7 <guyelazar7@gmail.com>
2023-05-10 18:55:09 +0300

    good day

--- opSystems/wet1/signals.cpp
+++ opSystems/wet1/signals.cpp
@@ -2,11 +2,15 @@
 #include <signal.h>
 #include "signals.h"
 #include "Commands.h"
-
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
+	cout<<"smash: got ctrl-Z"<<endl;
+  int pid = getpid();
+  cout<<"smash: process "<< pid <<" was stopped"<<endl;
 }
 
 void ctrlCHandler(int sig_num) {
